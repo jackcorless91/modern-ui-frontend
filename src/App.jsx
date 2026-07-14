1776f11d846e3a3b3c090a6b7d133afd90eaaacf
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2026-07-14 20:43:07 +1000

    created app.jsx scaffalding and component file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div className="min-h-screen bg-slate-950"></div>
+  return <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+
+    <Navbar />
+    <Hero />
+    <Features />
+    <Pricing />
+    <Testimonials />
+    <Footer />
+
+  </div>
 }
 
 export default App;
